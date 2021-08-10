6b53f2ba56c585172596ce4630a1413f4ed6f7d8
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-10 21:47:30 +0200

    updates

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -9,28 +9,29 @@ import pandas as pd
 import time
 from werkzeug.routing import BaseConverter
 
-root = "home/"
-aux_root = "home/"
+
 app = Flask(__name__, template_folder="./templates")
 
-app.config['STATIC_FOLDER'] = "home/"
+root = "home"
+aux_root = "home"
 
-uploads_dir = os.path.join(app.instance_path, 'uploads')
+app.secret_key = 'fat'
+
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
 
-
 app.url_map.converters['regex'] = RegexConverter
 
-
 @app.route("/")
+@app.route("/home")
 def index():
+    session['pth'] = root
     folders, files = get_file_tree()
     
-    session['pth'] = "/home/"
+    
     return render_template("index.html", folders = folders, files = files, cur_pth = session['pth']  )
 
 def get_file_tree():
@@ -38,9 +39,9 @@ def get_file_tree():
     folders  = []
     files = []
 
-    for f in os.listdir(app.config.get('STATIC_FOLDER')):
+    for f in os.listdir(session['pth']):
         
-        if os.path.isdir(app.config.get('STATIC_FOLDER')+f):
+        if os.path.isdir(os.path.join( session['pth'],f)):
             
             folders.append(f)
         else:
@@ -48,20 +49,21 @@ def get_file_tree():
                 
     return folders, files
 
-@app.route('/<regex("[abcABC0-9]{4,6}"):uid>-<slug>/<string:file>')
-def goto(filename):
-    return str(request.url)
-    app.config['STATIC_FOLDER'] = os.path.join(app.config.get('STATIC_FOLDER'), filename)
+
+@app.route('/<path:pth>/<string:file>')
+def goto(pth,file):
+    
+    session['pth'] =  request.url[25:]
     folders, files = get_file_tree()
-    
-    return render_template("index.html", folders = folders, files = files)
+    print(session['pth'])
+    return render_template("index.html", folders = folders, files = files, cur_pth = request.url[25:] )
     
 
-
-@app.route('/<regex("[A-Z|a-z]*")/<string:file>')
+'''
+@app.route('/<string:file>')
 def send(file):
     
-    return os.path.join(session['pth'], request.url)
+    return os.path.join(session['a'], request.url)
     try:
         return send_file(os.path.join(app.config.get('STATIC_FOLDER'),file), as_attachment= True)
         
@@ -69,7 +71,7 @@ def send(file):
         
         return "Something went wrong"
         
-    
+'''   
 
 
 if __name__ == '__main__':
