aa0da192acb4812aa67db4b073d510f5977e20cf
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 20:43:56 +0200

    using user

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -96,7 +96,7 @@ def login():
                 dropzone = Dropzone(app)
                 
                 folders, files = get_file_tree()
-                return redirect(url_for('goto',pth = session['pth'], file = folders))
+                return redirect(url_for('goto',pth = session['pth'], file = user))
                 
             else:
                 return "Pass missmatch"
