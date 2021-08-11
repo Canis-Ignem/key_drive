fef18a3318a425296e94788222ecafb791d9b27e
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 22:55:30 +0200

    back to normal

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -15,7 +15,18 @@ from flask_dropzone import Dropzone
 
 app = Flask(__name__, template_folder="./templates")
 
+root = "/home/keystone/"
+aux_root = "home"
+
 app.secret_key = 'fat'
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route("/")
 @app.route("/home")
@@ -147,6 +158,6 @@ def upload():
 
 if __name__ == '__main__':
 
-    app.run("192.168.1.44")
+    app.run("192.168.1.44/drive")
 
     
