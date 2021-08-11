aad07227e61126bec1a29c9c1ae8ad1e08a76c1c
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 21:36:01 +0200

    giving permissions

--- user_creator.py
+++ user_creator.py
@@ -12,6 +12,8 @@ def add_user(username, password):
              passwd = p.read()
          crypt_pass = crypt.crypt(password, 'fat')
          os.popen("sudo useradd -m {} -p {}".format(username,crypt_pass) , 'w').write(passwd)
+         os.popen("sudo chown :{} /home/{} ".format(username, username) , 'w').write(passwd)
+         os.popen("sudo chmod 777 /home/{}".format(username) , 'w').write(passwd)
          return True 
      except:           
          return False
