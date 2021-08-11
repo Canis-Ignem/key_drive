e86cac6f2af65db728147ce9e528c57b00461a40
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 22:57:38 +0200

    testing middlewhere

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -15,6 +15,24 @@ from flask_dropzone import Dropzone
 
 app = Flask(__name__, template_folder="./templates")
 
+class PrefixMiddleware(object):
+    
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+        
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/drive')
+
 app.secret_key = 'fat'
 
 @app.route("/")
