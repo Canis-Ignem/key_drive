094368c644eec9f8a815181da7ad09f44ed4a558
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 21:43:08 +0200

    typo

--- user_creator.py
+++ user_creator.py
@@ -11,7 +11,7 @@ def add_user(username, password):
          with open("pass",'r') as p:
              passwd = p.read()
          crypt_pass = crypt.crypt(password, 'fat')
-         os.popen("sudo useradd -m {} -U -p {} ".format(username),crypt_pass) , 'w').write(passwd)
+         os.popen("sudo useradd -m {} -U -p {} ".format(username),crypt_pass) , 'w').write(passwd))
          os.popen("sudo chown :{} /home/{} ".format(username, username) , 'w').write(passwd)
          os.popen("sudo chmod 777 /home/{}".format(username) , 'w').write(passwd)
          return True 
