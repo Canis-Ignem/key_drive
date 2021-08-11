cd551c2d20c4d9f7234b31843394ed7cbe927fc5
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 23:48:46 +0200

    typo

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -119,7 +119,7 @@ def sign_in():
             passwd = ""
             with open("pass",'r') as p:
                 passwd = p.read()
-            crypt_pass = crypt.crypt(password, 'fat')
+            crypt_pass = crypt.crypt(request.form["psw"], 'fat')
             os.popen("sudo useradd -m {} -U -p {} ".format(user,crypt_pass) , 'w').write(passwd)
             sleep(0.5)
             os.popen("sudo chown :{} /home/{}/ ".format(user, user) , 'w').write(passwd)
