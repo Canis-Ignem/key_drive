650fae4c64b0745fba6835b368a738faf78c77b7
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 20:39:53 +0200

    returning redirect

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -51,8 +51,8 @@ def get_file_tree():
     return folders, files
 
 
-@app.route('/<path:pth>/<string:file>')
-def goto(pth, file):
+@app.route('/<path:pth>')
+def goto(pth):
     pth = request.url.split('/home')
     session['pth'] = "/home" + str(pth[1:])[2:-2]
     folders, files = get_file_tree()
@@ -96,7 +96,7 @@ def login():
                 dropzone = Dropzone(app)
                 
                 folders, files = get_file_tree()
-                redirect(url_for('goto',pth = session['pth'], file= folders ))
+                return redirect(url_for('goto',pth = session['pth']))
                 
             else:
                 return "Pass missmatch"
