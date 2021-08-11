f80618e6b2d169460a2ea8b0dc0803a88c5d9bc3
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 23:47:41 +0200

    testing

--- db.py
+++ db.py
@@ -89,17 +89,17 @@ def get_grades(batch,email,ex):
 
 
 def add_linux_user(username, password):
-                         
-     try:
-         passwd = ""
-         with open("pass",'r') as p:
-             passwd = p.read()
-         crypt_pass = crypt.crypt(password, 'fat')
-         os.popen("sudo useradd -m {} -U -p {} ".format(username,crypt_pass) , 'w').write(passwd)
-         sleep(0.5)
-         os.popen("sudo chown :{} /home/{}/ ".format(username, username) , 'w').write(passwd)
-         sleep(0.5)
-         os.popen("sudo chmod 777 /home/{}/".format(username) , 'w').write(passwd)
-         return True 
-     except:           
-         return False
+    return True               
+    try:
+        passwd = ""
+        with open("pass",'r') as p:
+            passwd = p.read()
+        crypt_pass = crypt.crypt(password, 'fat')
+        os.popen("sudo useradd -m {} -U -p {} ".format(username,crypt_pass) , 'w').write(passwd)
+        sleep(0.5)
+        os.popen("sudo chown :{} /home/{}/ ".format(username, username) , 'w').write(passwd)
+        sleep(0.5)
+        os.popen("sudo chmod 777 /home/{}/".format(username) , 'w').write(passwd)
+        return True 
+    except:           
+        return False
