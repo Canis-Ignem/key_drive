d7ad6c9cc57e2a8548b55b93fe66c774af6ec29f
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-08-11 22:51:51 +0200

    prefix middle where

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -14,10 +14,27 @@ from md5 import md5
 
 from flask_dropzone import Dropzone
 
+class PrefixMiddleware(object):
+    
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
 app = Flask(__name__, template_folder="./templates")
 app.config["APPLICATION_ROOT"] = "/drive"
-root = "/home/keystone/"
-aux_root = "home"
+app.config["DROPZONE_UPLOAD_MULTIPLE"] = True
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/drive')
 
 app.secret_key = 'fat'
 
@@ -157,11 +174,6 @@ def upload():
         
     return render_template('index.html')
 
-def simple(env, resp):
-    resp(b'200 OK', [(b'Content-Type', b'text/plain')])
-    return [b'Hello WSGI World']
-
-app.wsgi_app = DispatcherMiddleware(simple, {'/drive': app.wsgi_app})
 if __name__ == '__main__':
 
     app.run("192.168.1.44")
