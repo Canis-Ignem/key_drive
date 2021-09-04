36fa10da5b8cdff82a81aa0eae6e657bdb926913
Canis Ignem <jonperezetxebarria@gmail.com>
2021-09-04 17:56:31 +0200

    ; out

--- md5.py
+++ md5.py
@@ -4,5 +4,5 @@ import hashlib as hs
 def md5(text):
         salt ="22"
         bytes = (text+salt).encode() 
-        readable_hash = hs.md5(bytes).hexdigest();
+        readable_hash = hs.md5(bytes).hexdigest()
         return readable_hash
